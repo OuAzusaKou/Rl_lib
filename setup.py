ff87d8e8d0c49b117e66d64af6eb2a2cabf5a26b
OuAzusaKou <940257123@qq.com>
2021-12-08 21:07:38 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Jueru",
+    version="0.1",
+    packages=find_packages(),
+    author='Zihang Wang, Jiayuan Li, Dunqi Yao',
+    url='https://github.com/OuAzusaKou/Rl_lib',
+    license='MIT'
+)
