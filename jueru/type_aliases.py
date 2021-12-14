07573540010775804869eca5f3b623c6f7763778
OuAzusaKou <940257123@qq.com>
2021-12-14 21:18:11 +0800

    modify dict support

--- jueru/type_aliases.py
+++ jueru/type_aliases.py
@@ -1,8 +1,7 @@
 from typing import Any, Callable, Dict, List, NamedTuple, Tuple, Union
 import gym
 
-from jueru import callbacks
+
 
 GymEnv = Union[gym.Env]
 Schedule = Callable[[float], float]
-MaybeCallback = Union[None, Callable, List[callbacks.BaseCallback], callbacks.BaseCallback]
