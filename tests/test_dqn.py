5712b16b3ab6d98f1b1ed5c0562faee11e3bcfac
OuAzusaKou <940257123@qq.com>
2021-12-15 12:03:06 +0800

    modify test modify pytest

--- tests/test_dqn.py
+++ tests/test_dqn.py
@@ -6,58 +6,60 @@ from jueru.algorithms import BaseAlgorithm, DQNAlgorithm
 from jueru.datacollection import Replay_buffer
 from jueru.updator import critic_updator_dqn, actor_updator_ddpg, soft_update
 from jueru.user.custom_actor_critic import MLPfeature_extractor, ddpg_critic, FlattenExtractor, dqn_critic
+import pytest
 
-env = gym.make('CartPole-v0')
+def test_dqn():
+    env = gym.make('CartPole-v0')
 
-feature_extractor = FlattenExtractor(env.observation_space)
+    feature_extractor = FlattenExtractor(env.observation_space)
 
-# actor = dqn_actor(env.action_space, feature_extractor, 3)
+    # actor = dqn_actor(env.action_space, feature_extractor, 3)
 
-critic = dqn_critic(env.action_space, feature_extractor, np.prod(env.observation_space.shape))
+    critic = dqn_critic(env.action_space, feature_extractor, np.prod(env.observation_space.shape))
 
-data_collection_dict = {}
+    data_collection_dict = {}
 
-data_collection_dict['replay_buffer'] = Replay_buffer(env=env, size=1e6)
+    data_collection_dict['replay_buffer'] = Replay_buffer(env=env, size=1e6)
 
-functor_dict = {}
+    functor_dict = {}
 
-lr_dict = {}
+    lr_dict = {}
 
-updator_dict = {}
+    updator_dict = {}
 
-functor_dict['critic'] = critic
+    functor_dict['critic'] = critic
 
-lr_dict['critic'] = 1e-3
+    lr_dict['critic'] = 1e-3
 
-updator_dict['critic_update'] = critic_updator_dqn
+    updator_dict['critic_update'] = critic_updator_dqn
 
-dqn = DQNAlgorithm(agent_class=DQN_agent,
-                   functor_dict=functor_dict,
-                   lr_dict=lr_dict,
-                   updator_dict=updator_dict,
-                   data_collection_dict=data_collection_dict,
-                   env=env,
-                   buffer_size=1e6,
-                   gamma=0.99,
-                   batch_size=100,
-                   tensorboard_log="./DQN_tensorboard",
-                   render=True,
-                   action_noise=0.1,
-                   min_update_step=1000,
-                   update_step=100,
-                   polyak=0.995,
-                   )
+    dqn = DQNAlgorithm(agent_class=DQN_agent,
+                       functor_dict=functor_dict,
+                       lr_dict=lr_dict,
+                       updator_dict=updator_dict,
+                       data_collection_dict=data_collection_dict,
+                       env=env,
+                       buffer_size=1e6,
+                       gamma=0.99,
+                       batch_size=100,
+                       tensorboard_log="./DQN_tensorboard",
+                       render=True,
+                       action_noise=0.1,
+                       min_update_step=1000,
+                       update_step=100,
+                       polyak=0.995,
+                       )
 
-dqn.learn(num_train_step=5000)
+    dqn.learn(num_train_step=5000)
 
-agent = DQN_agent.load('Base_model_address')
+    agent = DQN_agent.load('Base_model_address')
 
-obs = env.reset()
-for i in range(1001):
-    action = agent.predict(obs)
-    obs, reward, done, info = env.step(action)
-    env.render()
-    if done:
-      obs = env.reset()
+    obs = env.reset()
+    for i in range(1001):
+        action = agent.predict(obs)
+        obs, reward, done, info = env.step(action)
+        env.render()
+        if done:
+          obs = env.reset()
 
-env.close()
+    env.close()
--- tests/test_sac.py
+++ tests/test_sac.py
@@ -10,71 +10,72 @@ from jueru.updator import critic_updator_ddpg, actor_updator_ddpg, soft_update, 
 from jueru.user.custom_actor_critic import MLPfeature_extractor, ddpg_actor, ddpg_critic, FlattenExtractor, Sac_actor, \
     Sac_critic
 
-env = gym.make('Pendulum-v1')
 
-feature_extractor = FlattenExtractor(env.observation_space)
+def test_dqn():
+    env = gym.make('Pendulum-v1')
 
-actor = Sac_actor(
-    action_space=env.action_space, hidden_dim=128, feature_extractor=feature_extractor,
-    feature_dim=np.prod(env.observation_space.shape), log_std_min=-10, log_std_max=2
-)
+    feature_extractor = FlattenExtractor(env.observation_space)
 
-critic = Sac_critic(
-        action_space=env.action_space, feature_extractor=feature_extractor, hidden_dim=128,
-        feature_dim=np.prod(env.observation_space.shape)
+    actor = Sac_actor(
+        action_space=env.action_space, hidden_dim=128, feature_extractor=feature_extractor,
+        feature_dim=np.prod(env.observation_space.shape), log_std_min=-10, log_std_max=2
     )
 
-log_alpha = torch.tensor(np.log(0.01))
-log_alpha.requires_grad = True
+    critic = Sac_critic(
+            action_space=env.action_space, feature_extractor=feature_extractor, hidden_dim=128,
+            feature_dim=np.prod(env.observation_space.shape)
+        )
 
-Sac_agent = Sac_agent
+    log_alpha = torch.tensor(np.log(0.01))
+    log_alpha.requires_grad = True
 
-data_collection_dict = {}
+    data_collection_dict = {}
 
-data_collection_dict['replay_buffer'] = Replay_buffer(env=env, size=1e6)
+    data_collection_dict['replay_buffer'] = Replay_buffer(env=env, size=1e6)
 
-functor_dict = {}
+    functor_dict = {}
 
-lr_dict = {}
+    lr_dict = {}
 
-updator_dict = {}
+    updator_dict = {}
 
-functor_dict['actor'] = actor
+    functor_dict['actor'] = actor
 
-functor_dict['critic'] = critic
+    functor_dict['critic'] = critic
 
-functor_dict['critic_target'] = None
+    functor_dict['critic_target'] = None
 
-functor_dict['log_alpha'] = log_alpha
+    functor_dict['log_alpha'] = log_alpha
 
-lr_dict['actor'] = 1e-3
+    lr_dict['actor'] = 1e-3
 
-lr_dict['critic'] = 1e-3
+    lr_dict['critic'] = 1e-3
 
-lr_dict['critic_target'] = 1e-3
+    lr_dict['critic_target'] = 1e-3
 
-lr_dict['log_alpha'] = 1e-3
+    lr_dict['log_alpha'] = 1e-3
 
-updator_dict['actor_and_alpha_update'] = actor_and_alpha_updator_sac
+    updator_dict['actor_and_alpha_update'] = actor_and_alpha_updator_sac
 
-updator_dict['critic_update'] = critic_updator_sac
+    updator_dict['critic_update'] = critic_updator_sac
 
-updator_dict['soft_update'] = soft_update
+    updator_dict['soft_update'] = soft_update
 
-sac = SACAlgorithm(agent_class=Sac_agent,
-                     functor_dict=functor_dict,
-                     lr_dict=lr_dict,
-                     updator_dict=updator_dict,
-                     data_collection_dict=data_collection_dict,
-                     env=env,
-                     gamma=0.99,
-                     batch_size=100,
-                     tensorboard_log="./SAC_tensorboard",
-                     render=True,
-                     action_noise=0.1,
-                     min_update_step=1000,
-                     update_step=100,
-                     polyak=0.995,
-                     )
+    sac = SACAlgorithm(agent_class=Sac_agent,
+                         functor_dict=functor_dict,
+                         lr_dict=lr_dict,
+                         updator_dict=updator_dict,
+                         data_collection_dict=data_collection_dict,
+                         env=env,
+                         gamma=0.99,
+                         batch_size=100,
+                         tensorboard_log="./SAC_tensorboard",
+                         render=True,
+                         action_noise=0.1,
+                         min_update_step=1000,
+                         update_step=100,
+                         polyak=0.995,
+                         save_interval=2000,
+                         )
 
-sac.learn(num_train_step=1000000,actor_update_freq=2)
+    sac.learn(num_train_step=3500, actor_update_freq=2)
