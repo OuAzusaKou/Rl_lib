349a9e8ab74f1a5b1979c1d8af754d6a52cd68c3
OuAzusaKou <940257123@qq.com>
2022-02-12 20:41:13 +0800

    modify utils

--- jueru/utils.py
+++ jueru/utils.py
@@ -1,5 +1,6 @@
 import glob
 import os
+from collections import deque
 
 import torch
 import torch as th
@@ -314,3 +315,32 @@ def get_latest_run_id(log_path: Optional[str] = None, log_name: str = "") -> int
         if log_name == "_".join(file_name.split("_")[:-1]) and ext.isdigit() and int(ext) > max_run_id:
             max_run_id = int(ext)
     return max_run_id
+
+class FrameStack(gym.Wrapper):
+    def __init__(self, env, k):
+        gym.Wrapper.__init__(self, env)
+        self._k = k
+        self._frames = deque([], maxlen=k)
+        shp = env.observation_space.shape
+        self.observation_space = gym.spaces.Box(
+            low=0,
+            high=1,
+            shape=((shp[0] * k,) + shp[1:]),
+            dtype=env.observation_space.dtype
+        )
+        self._max_episode_steps = env._max_episode_steps
+
+    def reset(self):
+        obs = self.env.reset()
+        for _ in range(self._k):
+            self._frames.append(obs)
+        return self._get_obs()
+
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        self._frames.append(obs)
+        return self._get_obs(), reward, done, info
+
+    def _get_obs(self):
+        assert len(self._frames) == self._k
+        return np.concatenate(list(self._frames), axis=0)
